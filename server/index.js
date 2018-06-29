f34ac938fc6488c327fdb597a91f0a6bc996de17
L.W.L <102495553@qq.com>
2018-06-29 15:59:38 +0800

    feat: add koa-views pug

--- server/index.js
+++ server/index.js
@@ -1,13 +1,14 @@
 const Koa = require('koa')
-const ejs = require('ejs')
-const pug = require('pug')
-const { htmlTpl, ejsTpl, pugTpl } = require('./tpl')
-
+const views = require('koa-views')
+const { resolve } = require('path')
 const app = new Koa()
 
+app.use(views(resolve(__dirname, './views'), {
+  extension: 'pug'
+}))
+
 app.use(async (ctx, next) => {
-  ctx.type = 'text/html; charset=utf-8'
-  ctx.body = pug.render(pugTpl, {
+  await ctx.render('index', {
     me: 'Jason',
     you: 'Wenli'
   })
